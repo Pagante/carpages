dca05d3c7a1c4d23942958a79037a4ded0aceacc
Pagante <55301195+Pagante@users.noreply.github.com>
2021-02-13 11:38:17 +0100

    completed team models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    def Thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50%" />'.format(object.photo.url))
+
+    Thumbnail.short_description = 'Image'
+    list_display = ('id','Thumbnail', 'first_name','last_name', 'designation', 'created_date')
+    list_display_links = ('id','Thumbnail', 'first_name',)
+    list_filter = ('designation',)
+
+    search_fields = ('id', 'first_name','last_name', 'designation')
+
+admin.site.register(Team, TeamAdmin)
